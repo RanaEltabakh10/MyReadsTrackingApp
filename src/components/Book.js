c99c463256867284982bcb8039fae25a597e0854
rana <ranamohamedeltabakh10@google.com>
2022-07-14 23:59:37 +0200

    first commit

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,7 +10,7 @@ const Book = ({ book, changeBookShelf }) => {
                 <div className="book-shelf-changer">
                     <select defaultValue={book.shelf ? book.shelf : "none"} onChange={(e) => changeBookShelf(book, e.target.value)}>
                         <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="currentlyReading">Currently  Reading</option>
                         <option value="wantToRead">Want to Read</option>
                         <option value="read">Read</option>
                         <option value="none">None</option>
